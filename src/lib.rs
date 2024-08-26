3ea95e9bd8eb400b1c0cefb43c36b455bceb1b7a
rajan-gith <rajan.beryls@gmail.com>
2024-08-26 22:37:12 +0530

    Init

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::{error, fs};
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not Enough params");
+        }
+        let query = args.get(1).unwrap().clone();
+        let file_path = args.get(2).unwrap().clone();
+
+        Ok(Self { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(&query, &contents));
+    }
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // let config = match Config::build(&args) {
+    //     Ok(config) => config,
+    //     Err(msg) => {
+    //         println!("{msg}");
+    //         process::exit(1);
+    //     }
+    // };
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {:?} in {:?}", config.query, config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
